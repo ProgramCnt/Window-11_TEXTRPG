468da2c888b0fecfb28b378097389995721ff546
ProgramCnt <corfafa2@naver.com>
2025-02-06 20:24:04 +0900

    Feat: 추가 던전매니저

--- Window-11_TEXTRPG/DungeonManager.cs
+++ Window-11_TEXTRPG/DungeonManager.cs
@@ -8,9 +8,42 @@ namespace Window_11_TEXTRPG
 {
     public class DungeonManager : IScene
     {
+        //빈 몬스터 배열 추가
+        List<Monster> monsters = new List<Monster>();
+
         public void Enter()
         {
-            
+            //플레이어 입력
         }
+
+        //몬스터 생성
+        public void SetMonsters() 
+        {
+        
+        }
+
+        //몬스터 타겟 지정
+        public void TargetMonster(int index)
+        {
+
+        }
+
+        //몬스터 공격
+        public void MonsterAttck(Monster monster)
+        {
+
+        }
+
+        //전투 패배
+        public void Lose()
+        {
+
+        }
+
+        //전투 승리
+        public void Victory()
+        {
+
+        } 
     }
 }
