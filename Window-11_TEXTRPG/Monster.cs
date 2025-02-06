f7d3f490db2d2665cb2c4ff8e31d7de2176a4a8f
ProgramCnt <corfafa2@naver.com>
2025-02-06 19:53:41 +0900

    Feat: 추가 몬스터 클래스

---
+++ Window-11_TEXTRPG/Monster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Window_11_TEXTRPG
+{
+    public class Monster
+    {
+        string name { get; set; }
+        int level { get; set; }
+        string type { get; set; }
+        int hp { get; set; }
+
+        public bool IsDie() 
+        {
+            if (hp <= 0) 
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
